8d262923ea09dfd1ff0c80127b41bdd031c7e9ac
htuli77 <hitesh.tuli@gmail.com>
2019-12-10 17:52:29 +1300

    commit in hotfix branch

--- src/main/java/org/sonatype/mavenbook/weather/Main.java
+++ src/main/java/org/sonatype/mavenbook/weather/Main.java
@@ -5,6 +5,7 @@ public class Main {
 public static void main(String[] args) throws Exception {
 System.out.println("Hello World");
 System.out.println("On Master Branch");
+System.out.println("Hotfix Branch");
 // Configure Log4J
 PropertyConfigurator
 .configure(Main.class.getClassLoader()
